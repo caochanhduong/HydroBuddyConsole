a8e5c2b108a13f5d01977ea244a36319ca3c6d96
Cao Chánh Dương <41425919+caochanhduong@users.noreply.github.com>
2018-08-02 11:00:50 +0700

    Add files via upload

--- demo.js
+++ demo.js
@@ -66,93 +66,103 @@ console.log("                    * Thử nghiệm trên tập hợp các loại 
 console.log("                    * Xin mời nhập các giá trị dưỡng chất sau:\n");
 var NO3 = readline.question("*********************Nhập hàm lượng N(NO3-):********************* \n");
 input[0]=parseFloat(NO3);
-var NH4 = readline.question("*********************Nhập hàm lượng N(NH4+):********************* \n");
-input[1]=parseFloat(NH4);
+var K = readline.question("*********************Nhập hàm lượng K:********************* \n");
+input[1]=parseFloat(K);
+
 var P = readline.question("*********************Nhập hàm lượng P:********************* \n");
 input[2]=parseFloat(P);
-var K = readline.question("*********************Nhập hàm lượng K:********************* \n");
-input[3]=parseFloat(K);
+
 var Mg = readline.question("*********************Nhập hàm lượng Mg:********************* \n");
-input[4]=parseFloat(Mg);
+input[3]=parseFloat(Mg);
 var Ca = readline.question("*********************Nhập hàm lượng Ca:********************* \n");
-input[5]=parseFloat(Ca);
+input[4]=parseFloat(Ca);
 var S = readline.question("*********************Nhập hàm lượng S:********************* \n");
-input[6]=parseFloat(S);
+input[5]=parseFloat(S);
 var Fe = readline.question("*********************Nhập hàm lượng Fe:********************* \n");
-input[7]=parseFloat(Fe);
+input[6]=parseFloat(Fe);
 var Zn = readline.question("*********************Nhập hàm lượng Zn:********************* \n");
-input[8]=parseFloat(Zn);
+input[7]=parseFloat(Zn);
 var Bo = readline.question("*********************Nhập hàm lượng B:********************* \n");
-input[9]=parseFloat(Bo);
-var Mn = readline.question("*********************Nhập hàm lượng Mn:********************* \n");
-input[10]=parseFloat(Mn);
+input[8]=parseFloat(Bo);
 var Cu = readline.question("*********************Nhập hàm lượng Cu:********************* \n");
-input[11]=parseFloat(Cu);
+input[9]=parseFloat(Cu);
 var Mo = readline.question("*********************Nhập hàm lượng Mo:********************* \n");
-input[12]=parseFloat(Mo);
+input[10]=parseFloat(Mo);
 var Na = readline.question("*********************Nhập hàm lượng Na:********************* \n");
-input[13]=parseFloat(Na);
+input[11]=parseFloat(Na);
 var Si = readline.question("*********************Nhập hàm lượng Si:********************* \n");
-input[14]=parseFloat(Si);
+input[12]=parseFloat(Si);
 var Cl = readline.question("*********************Nhập hàm lượng Cl:********************* \n");
-input[15]=parseFloat(Cl);
+input[13]=parseFloat(Cl);
+var Mn = readline.question("*********************Nhập hàm lượng Mn:********************* \n");
+input[14]=parseFloat(Mn);
+var NH4 = readline.question("*********************Nhập hàm lượng N(NH4+):********************* \n");
+input[15]=parseFloat(NH4);
 var ec_contribution=new Array(16);
 ec_contribution[0] = 71.46 / 14;
-ec_contribution[1] = 73 / 39;
+ec_contribution[1] = 106 / 24.30;
 ec_contribution[2] = 57 / 31;
-ec_contribution[3] = 106 / 24.30;
-ec_contribution[4] = 119 / 40;
-ec_contribution[5] = 160 / 32;
-ec_contribution[6] = 108.0 / 56;
+ec_contribution[3] = 119 / 40;
+ec_contribution[4] = 160 / 32;
+ec_contribution[5] = 108.0 / 56;
+ec_contribution[6] = 0;
 ec_contribution[7] = 0;
 ec_contribution[8] = 0;
-ec_contribution[9] = 0;
-ec_contribution[10]= 0;
-ec_contribution[11]= 50.01 / 23;
-ec_contribution[12]= 100 / 28.09;
-ec_contribution[13]= 76.35 / 35.5;
+ec_contribution[9]= 50.01 / 23;
+ec_contribution[10]= 100 / 28.09;
+ec_contribution[11]= 76.35 / 35.5;
+ec_contribution[12]= 0;
+ec_contribution[13]= 73.5 / 14;
 ec_contribution[14]= 0;
-ec_contribution[15]= 73.5 / 14;
+ec_contribution[15] = 73 / 39;
 
 //tạo ma trận tính toán
 var A=[[0,0,1.186,0.948,0,1.093,0,0,1.3856,0],
-        [1.4294,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0.9483,0,0],
         [0,0,0,0,0,0,0,0,3.867,0],
+        [0,0,0,0,0,0,0,0.9483,0,0],
         [0,0,0,0,0,0.948,0,0,0,0],
         [0,0,1.697,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,1.8973,0,0,1.624],
         [0,0,0,0,0.7,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,3.311],
         [0,1.7482,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,3.2504,0,0,0],
         [4.8943,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0]];
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,3.2504,0,0,0],
+        [1.4294,0,0,0,0,0,0,0,0,0]];
         var A1=[[0,0,1.186,0.948,0,1.093,0,0,1.3856,0],
-        [1.4294,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0.9483,0,0],
         [0,0,0,0,0,0,0,0,3.867,0],
+        [0,0,0,0,0,0,0,0.9483,0,0],
         [0,0,0,0,0,0.948,0,0,0,0],
         [0,0,1.697,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,1.8973,0,0,1.624],
         [0,0,0,0,0.7,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,3.311],
         [0,1.7482,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,3.2504,0,0,0],
         [0,0,0,2.149,0,0,0,0,0,0],
         [4.8943,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0]];
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,3.2504,0,0,0],
+        [1.4294,0,0,0,0,0,0,0,0,0]];
 var B=new Array(15);
 
 
 for(var i=0;i<15;i++)
 {
-    B[i]=input[i];
+    if(i<9)
+    {
+        B[i]=input[i];
+    }
+    else 
+    {
+        B[i]=input[i+1];
+    }
 }
+
 
 var x=new Array(10);
 
@@ -215,20 +225,122 @@ console.log("*******************************************************************
 console.log("*     CHẤT     *       LT(ppm)      *   KQ(ppm)  *      error(%)   *");
 console.log("**************************************************************************");
 console.log("*   N(NO3-)    *       "+input[0]+"       *     "+output[0]+"       *     "+error[0]+"    *");
-console.log("*   N(NH4+)    *       "+input[1]+"       *     "+output[1]+"       *     "+error[1]+"    *");
+console.log("*      K       *       "+input[1]+"       *     "+output[1]+"       *     "+error[1]+"    *");
 console.log("*      P       *       "+input[2]+"       *     "+output[2]+"       *     "+error[2]+"    *");
-console.log("*      K       *       "+input[3]+"       *     "+output[3]+"       *     "+error[3]+"    *");
-console.log("*      Mg      *       "+input[4]+"       *     "+output[4]+"       *     "+error[4]+"    *");
-console.log("*      Ca      *       "+input[5]+"       *     "+output[5]+"       *     "+error[5]+"    *");
-console.log("*      S       *       "+input[6]+"       *     "+output[6]+"       *     "+error[6]+"    *");
-console.log("*      Fe      *       "+input[7]+"       *     "+output[7]+"       *     "+error[7]+"    *");
-console.log("*      Zn      *       "+input[8]+"       *     "+output[8]+"       *     "+error[8]+"    *");
-console.log("*      B       *       "+input[9]+"       *     "+output[9]+"       *     "+error[9]+"    *");
-console.log("*      Mn      *       "+input[10]+"       *     "+output[10]+"       *     "+error[10]+"    *");
-console.log("*      Cu      *       "+input[11]+"       *     "+output[11]+"       *     "+error[11]+"    *");
-console.log("*      Mo      *       "+input[12]+"       *     "+output[12]+"       *     "+error[12]+"    *");
-console.log("*      Na      *       "+input[13]+"       *     "+output[13]+"       *     "+error[13]+"    *");
-console.log("*      Si      *       "+input[14]+"       *     "+output[14]+"       *     "+error[14]+"    *");
-console.log("*      Cl      *       "+input[15]+"       *     "+output[15]+"       *     "+error[15]+"    *");
+console.log("*      Mg      *       "+input[3]+"       *     "+output[3]+"       *     "+error[3]+"    *");
+console.log("*      Ca      *       "+input[4]+"       *     "+output[4]+"       *     "+error[4]+"    *");
+console.log("*      S       *       "+input[5]+"       *     "+output[5]+"       *     "+error[5]+"    *");
+console.log("*      Fe      *       "+input[6]+"       *     "+output[6]+"       *     "+error[6]+"    *");
+console.log("*      Zn      *       "+input[7]+"       *     "+output[7]+"       *     "+error[7]+"    *");
+console.log("*      B       *       "+input[8]+"       *     "+output[8]+"       *     "+error[8]+"    *");
+console.log("*      Cu      *       "+input[9]+"       *     "+output[9]+"       *     "+error[9]+"    *");
+console.log("*      Mo      *       "+input[10]+"       *     "+output[10]+"       *     "+error[10]+"    *");
+console.log("*      Na      *       "+input[11]+"       *     "+output[11]+"       *     "+error[11]+"    *");
+console.log("*      Si      *       "+input[12]+"       *     "+output[12]+"       *     "+error[12]+"    *");
+console.log("*      Cl      *       "+input[13]+"       *     "+output[13]+"       *     "+error[13]+"    *");
+console.log("*      Mn      *       "+input[14]+"       *     "+output[14]+"       *     "+error[14]+"    *");
+console.log("*   N(NH4+)    *       "+input[15]+"       *     "+output[15]+"       *     "+error[15]+"    *");
 console.log("**************************************************************************");
 
+console.log("\n\n******Dựa vào tên của các loại phân đã biết, ta có thể chia thành số bồn tối thiểu là 2 bồn \n");
+console.log("******Trong đó bồn A gồm những chất sau:");
+console.log("******"+name[2]+" , "+name[4]+" , "+name[5]+" , "+name[8]+"\n");
+console.log("******Trong đó bồn B gồm những chất sau:");
+console.log("******"+name[0]+" , "+name[1]+" , "+name[3]+" , "+name[6]+" , "+name[7]+" , "+name[9]+"\n");
+var sobon_nhap = readline.question("*********************Còn bạn muốn sử dụng bao nhiêu bồn?\n");
+var sobon=parseInt(sobon_nhap);
+var option = readline.question("*********************Bạn muốn nhập theo option nào(1/2) ?\n    1.Theo số cây\n    2.Theo mật độ cây trên hecta và diện tích trồng\n ");
+var opt=parseInt(option);
+while(opt!=1&&opt!=2)
+{
+    console.log("Cú pháp đã sai. Mời bạn nhập lại.\n")
+    var option_temp = readline.question("*********************Bạn muốn nhập theo option nào(1/2) ?\n    1.Theo số cây\n    2.Theo mật độ cây trên hecta và diện tích trồng\n ");
+    opt=parseInt(option_temp);
+}
+if(opt==1)
+{
+    var numTree = readline.question("*********************Nhập số cây:********************* \n");
+    var soCay=parseInt(numTree);
+}
+else if(opt==2)
+{
+    var numTree_ha = readline.question("*********************Mật độ cây trên hecta (số cây/hecta):********************* \n");
+    var soCay_ha=parseFloat(numTree_ha);
+    var dientich = readline.question("*********************Nhập diện tích trồng:********************* \n");
+    var dienTichTrong=parseFloat(dientich);
+    var soCay1=parseInt(dienTichTrong/soCay_ha);
+}
+
+
+var percent=new Array(10);
+var phan0 = readline.question("*********************Nhập % so với độ bão hòa của phân "+name[0]+"********************* \n");
+percent[0]=parseFloat(phan0);
+var phan1 = readline.question("*********************Nhập % so với độ bão hòa của phân "+name[1]+"********************* \n");
+percent[1]=parseFloat(phan1);
+var phan2 = readline.question("*********************Nhập % so với độ bão hòa của phân "+name[2]+"********************* \n");
+percent[2]=parseFloat(phan2);
+var phan3 = readline.question("*********************Nhập % so với độ bão hòa của phân "+name[3]+"********************* \n");
+percent[3]=parseFloat(phan3);
+var phan4 = readline.question("*********************Nhập % so với độ bão hòa của phân "+name[4]+"********************* \n");
+percent[4]=parseFloat(phan4);
+var phan5 = readline.question("*********************Nhập % so với độ bão hòa của phân "+name[5]+"********************* \n");
+percent[5]=parseFloat(phan5);
+var phan6 = readline.question("*********************Nhập % so với độ bão hòa của phân "+name[6]+"********************* \n");
+percent[6]=parseFloat(phan6);
+var phan7 = readline.question("*********************Nhập % so với độ bão hòa của phân "+name[7]+"********************* \n");
+percent[7]=parseFloat(phan7);
+var phan8 = readline.question("*********************Nhập % so với độ bão hòa của phân "+name[8]+"********************* \n");
+percent[8]=parseFloat(phan8);
+var phan9 = readline.question("*********************Nhập % so với độ bão hòa của phân "+name[9]+"********************* \n");
+percent[9]=parseFloat(phan9);
+var litMotBon=100.0;
+var baohoa=new Array(10);//nồng độ bão hòa gốc
+baohoa[0]=200.1;
+baohoa[1]=200.1;
+baohoa[2]=200.1;
+baohoa[3]=200.1;
+baohoa[4]=200.1;
+baohoa[5]=200.1;
+baohoa[6]=200.1;
+baohoa[7]=200.1;
+baohoa[8]=200.1;
+baohoa[9]=200.1;
+var nongdo=new Array(10) //Nồng độ thực tế
+for(var i=0;i<10;i++)
+{
+    nongdo[i]=percent[i]*baohoa[i];
+}
+//tính số lượng phân cần
+var khoiLuongCan=new Array(10);
+if(opt==1)//dùng socay
+{
+    for(var i=0;i<10;i++)
+    {
+        khoiLuongCan[i]=x[i]*soCay;
+    }
+}
+else//dùng socay1
+{
+    for(var i=0;i<10;i++)
+    {
+        khoiLuongCan[i]=x[i]*soCay1;
+    }
+}
+var luongNuocCan=new Array(10);//tính được lượng nước cần cho mỗi loại cây
+for (var i=0;i<10;i++)
+{
+    luongNuocCan[i]=(khoiLuongCan[i]/nongdo[i])*100/1000;
+}
+//tính tổng nước
+var tongNuoc=0;
+for(var i=0;i<10;i++)
+{
+    tongNuoc+=luongNuocCan[i];
+}
+//xuất khối lượng từng loại phân và tổng nước
+for(var i=0;i<10;i++)
+{
+    console.log("Khối lượng phân "+name[i]+" cần pha là: "+khoiLuongCan[i]/1000+"kg");
+
+}
+console.log("\n Tổng nước là:"+tongNuoc+" lít");
